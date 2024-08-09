3516ee20e1b327e2cfee41e66a45bb325bd33956
Josh Meyers <jmeyers314@gmail.com>
2024-08-09 10:11:37 -0700

    Cleanup

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-// Global twilight and moon data
+// Keep these script-global
 let filteredTwilightData = [];
 let filteredMoonData = [];
 let filteredObservationData = [];
 
-// Select the SVG element
 const svg = d3.select("#mySVG");
 
-// Define the margins and dimensions
 const margin = { top: 20, right: 20, bottom: 20, left: 60 };
 const width = +svg.attr("width") - margin.left - margin.right;
 const height = +svg.attr("height") - margin.top - margin.bottom;
 
+const g = svg.append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// x-axis
+//  Time range for x-axis.  17:00 to 07:30
 const minTime = -5;
 const maxTime = 7.5;
-
-// Append a group element to the SVG and transform it based on the margins
-const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
-
-// Define the custom time range for the x-axis (from -5 hours to +7.5 hours)
 const customTimeScale = d3.scaleLinear()
     .domain([minTime, maxTime])
     .range([0, width]);
 
-// Define the date range for the y-axis
-const dateRange = d3.timeDay.range(new Date("2024-10-01"), new Date("2024-12-01"));
+// y-axis
+const dateStart = new Date("2024-10-01");
+const dateEnd = new Date("2024-12-01");
+const dateRange = d3.timeDay.range(dateStart, dateEnd);
 const dateScale = d3.scaleBand()
     .domain(dateRange.map(d => d.toISOString().split("T")[0]))
     .range([0, height])
     .padding(0.01);
 
-// Define a color scale for sun states
 const sunStateColorScale = d3.scaleOrdinal()
     .domain(["day", "6-deg twilight", "12-deg twilight", "18-deg twilight", "night"])
     .range(["#EEEE00", "#00FFFF", "#00AAAA", "#005555", "#000000"]);
 
-// Define a color scale for observation categories
 const observationColorScale = d3.scaleOrdinal()
     .domain(["Calibration", "Prep", "AOS transient", "AOS data", "IQ", "Science"])
     .range(["#FF0000", "#FF0000", "#FFFF00", "#0000FF", "#00FF00", "#00FFFF"]);
@@ -47,7 +45,7 @@ function formatTime(hoursDecimal) {
     let hours = Math.floor(hoursDecimal);
     let minutes = Math.round((hoursDecimal - hours) * 60);
 
-    // Adjust hours and minutes to be within 24-hour format
+    // Adjust decimal hours to be +ve hh:mm format
     if (hours < 0) {
         hours += 24;
     } else if (hours >= 24) {
@@ -63,12 +61,14 @@ function formatTooltip(obs, moonIllumination) {
     const durationHours = obs.end_time - obs.start_time;
     const duration = formatTime(durationHours);
 
-    return `${obs.date}<br>${obs.tooltip}<br>Start: ${startTime}<br>End: ${endTime}<br>Duration: ${duration}<br>Moon Illumination: ${(moonIllumination * 100).toFixed(2)}%`;
+    return `${obs.date}<br>${obs.tooltip}<br>Start: ${startTime}<br>` +
+           `End: ${endTime}<br>Duration: ${duration}<br>` +
+           `Moon Illumination: ${(moonIllumination * 100).toFixed(2)}%`;
 }
 
 function renderObservations() {
     const padding = 3; // Horizontal padding for the rectangles
-    const cornerRadius = 3; // Radius for rounded corners
+    const cornerRadius = 3;
 
     g.selectAll(".observation").remove(); // Clear previous observations
 
@@ -82,7 +82,9 @@ function renderObservations() {
         const rectHeight = dateScale.bandwidth() * 0.8;
         const rectY = dateScale(obs.date) + dateScale.bandwidth() * 0.1;
 
-        const group = link.append("g").attr("class", "observation").attr("data-index", i);
+        const group = link.append("g")
+            .attr("class", "observation")
+            .attr("data-index", i);
 
         group.append("rect")
             .attr("x", xStart)
@@ -100,21 +102,23 @@ function renderObservations() {
             .attr("dy", ".35em") // Adjusts the text vertically to be centered
             .attr("text-anchor", "middle") // Centers the text horizontally
             .attr("fill", "#FFFFFF") // Text color
-            .style("font-family", "monospace") // Sets the font to monospace
-            .style("font-size", "10px") // Sets the font size to 10px (or any desired size)
+            .style("font-family", "monospace")
+            .style("font-size", "10px")
             .text(obs.label);
 
         // Find the corresponding moon data for the current observation date
         const moonDataForDate = filteredMoonData.find(moon => moon.date === obs.date);
 
         group.on("mouseover", function(event) {
-                const tooltip = d3.select("#tooltip");
-                tooltip.style("display", "block")
-                    .style("left", (event.pageX + 5) + "px")
-                    .style("top", (event.pageY + 5) + "px")
-                    .style("font-family", "monospace")
-                    .style("font-size", "10px")
-                    .html(formatTooltip(obs, moonDataForDate ? moonDataForDate.illumination : 0));
+            const tooltip = d3.select("#tooltip");
+            tooltip.style("display", "block")
+                .style("left", (event.pageX + 5) + "px")
+                .style("top", (event.pageY + 5) + "px")
+                .style("font-family", "monospace")
+                .style("font-size", "10px")
+                .html(formatTooltip(
+                    obs, moonDataForDate ? moonDataForDate.illumination : 0
+                ));
             })
             .on("mousemove", function(event) {
                 const tooltip = d3.select("#tooltip");
@@ -130,16 +134,21 @@ function renderObservations() {
 
                 // Highlight the selected observation
                 if (!isSelected) {
-                    d3.select(this).select("rect").attr("stroke", "yellow").attr("stroke-width", 2);
+                    d3.select(this)
+                      .select("rect")
+                      .attr("stroke", "yellow")
+                      .attr("stroke-width", 2);
                 } else {
-                    d3.select(this).select("rect").attr("stroke", null).attr("stroke-width", null);
+                    d3.select(this)
+                      .select("rect")
+                      .attr("stroke", null)
+                      .attr("stroke-width", null);
                 }
             });
     });
 }
 
 function renderAxes() {
-    // Add axes
     const xAxis = d3.axisBottom(customTimeScale).tickFormat(d => {
         const hours = Math.floor(d);
         const minutes = (d - hours) * 60;
@@ -167,92 +176,135 @@ function loadObservations() {
         const [twilightData, moonData, observationData] = data;
 
         // Filter data to exclude dates outside the specified range
-        filteredTwilightData = twilightData.filter(d => dateRange.map(date => date.toISOString().split("T")[0]).includes(d.date));
-        filteredMoonData = moonData.filter(d => dateRange.map(date => date.toISOString().split("T")[0]).includes(d.date));
-        filteredObservationData = observationData.filter(d => dateRange.map(date => date.toISOString().split("T")[0]).includes(d.date));
+        filteredTwilightData = twilightData
+            .filter(d => dateRange.map(
+                date => date.toISOString().split("T")[0]).includes(d.date)
+            );
+        filteredMoonData = moonData
+            .filter(d => dateRange.map(
+                date => date.toISOString().split("T")[0]).includes(d.date)
+            );
+        filteredObservationData = observationData
+            .filter(d => dateRange.map(
+                date => date.toISOString().split("T")[0]).includes(d.date)
+            );
 
-        // Add background rectangles for sun states
+        // Add twilight rectangles; only need to do this once.
         filteredTwilightData.forEach(state => {
+            let wafternoon = (
+                customTimeScale(state.sunset) -
+                customTimeScale(minTime)
+            );
             g.append("rect")
                 .attr("class", "twilight")
                 .attr("x", customTimeScale(minTime))
                 .attr("y", dateScale(state.date))
-                .attr("width", customTimeScale(state.sunset) - customTimeScale(minTime))
+                .attr("width", wafternoon)
                 .attr("height", dateScale.bandwidth())
                 .attr("fill", sunStateColorScale("day"));
 
+            let we6deg = (
+                customTimeScale(state.evening_6deg) -
+                customTimeScale(state.sunset)
+            );
             g.append("rect")
                 .attr("class", "twilight")
                 .attr("x", customTimeScale(state.sunset))
                 .attr("y", dateScale(state.date))
-                .attr("width", customTimeScale(state.evening_6deg) - customTimeScale(state.sunset))
+                .attr("width", we6deg)
                 .attr("height", dateScale.bandwidth())
                 .attr("fill", sunStateColorScale("6-deg twilight"));
 
+            let we12deg = (
+                customTimeScale(state.evening_12deg) -
+                customTimeScale(state.evening_6deg)
+            );
             g.append("rect")
                 .attr("class", "twilight")
                 .attr("x", customTimeScale(state.evening_6deg))
                 .attr("y", dateScale(state.date))
-                .attr("width", customTimeScale(state.evening_12deg) - customTimeScale(state.evening_6deg))
+                .attr("width", we12deg)
                 .attr("height", dateScale.bandwidth())
                 .attr("fill", sunStateColorScale("12-deg twilight"));
 
+            let we18deg = (
+                customTimeScale(state.evening_18deg) -
+                customTimeScale(state.evening_12deg)
+            );
             g.append("rect")
                 .attr("class", "twilight")
                 .attr("x", customTimeScale(state.evening_12deg))
                 .attr("y", dateScale(state.date))
-                .attr("width", customTimeScale(state.evening_18deg) - customTimeScale(state.evening_12deg))
+                .attr("width", we12deg)
                 .attr("height", dateScale.bandwidth())
                 .attr("fill", sunStateColorScale("18-deg twilight"));
 
+            let wnight = (
+                customTimeScale(state.morning_18deg) -
+                customTimeScale(state.evening_18deg)
+            );
             g.append("rect")
                 .attr("class", "twilight")
                 .attr("x", customTimeScale(state.evening_18deg))
                 .attr("y", dateScale(state.date))
-                .attr("width", customTimeScale(state.morning_18deg) - customTimeScale(state.evening_18deg))
+                .attr("width", wnight)
                 .attr("height", dateScale.bandwidth())
                 .attr("fill", sunStateColorScale("night"));
 
+            let wm18deg = (
+                customTimeScale(state.morning_12deg) -
+                customTimeScale(state.morning_18deg)
+            );
             g.append("rect")
                 .attr("class", "twilight")
                 .attr("x", customTimeScale(state.morning_18deg))
                 .attr("y", dateScale(state.date))
-                .attr("width", customTimeScale(state.morning_12deg) - customTimeScale(state.morning_18deg))
+                .attr("width", wm18deg)
                 .attr("height", dateScale.bandwidth())
                 .attr("fill", sunStateColorScale("18-deg twilight"));
 
+            let wm12deg = (
+                customTimeScale(state.morning_6deg) -
+                customTimeScale(state.morning_12deg)
+            );
             g.append("rect")
                 .attr("class", "twilight")
                 .attr("x", customTimeScale(state.morning_12deg))
                 .attr("y", dateScale(state.date))
-                .attr("width", customTimeScale(state.morning_6deg) - customTimeScale(state.morning_12deg))
+                .attr("width", wm12deg)
                 .attr("height", dateScale.bandwidth())
                 .attr("fill", sunStateColorScale("12-deg twilight"));
 
+            let wm6deg = (
+                customTimeScale(state.sunrise) -
+                customTimeScale(state.morning_6deg)
+            );
             g.append("rect")
                 .attr("class", "twilight")
                 .attr("x", customTimeScale(state.morning_6deg))
                 .attr("y", dateScale(state.date))
-                .attr("width", customTimeScale(state.sunrise) - customTimeScale(state.morning_6deg))
+                .attr("width", wm6deg)
                 .attr("height", dateScale.bandwidth())
                 .attr("fill", sunStateColorScale("6-deg twilight"));
 
+            let wmorning = (
+                customTimeScale(maxTime) -
+                customTimeScale(state.sunrise)
+            );
             g.append("rect")
                 .attr("class", "twilight")
                 .attr("x", customTimeScale(state.sunrise))
                 .attr("y", dateScale(state.date))
-                .attr("width", customTimeScale(maxTime) - customTimeScale(state.sunrise))
+                .attr("width", wmorning)
                 .attr("height", dateScale.bandwidth())
                 .attr("fill", sunStateColorScale("day"));
         });
 
-        // Add rectangles for moon states
+        // Moon rectangles; only needed once.
         filteredMoonData.forEach(state => {
             state.moonintervals.forEach(interval => {
                 const start = Math.max(minTime, interval[0]);
                 const end = Math.min(maxTime, interval[1]);
-
-                // Only add the rectangle if the interval is within the valid range
                 if (start < end) {
                     g.append("rect")
                         .attr("class", "moon")
@@ -261,40 +313,42 @@ function loadObservations() {
                         .attr("width", customTimeScale(end) - customTimeScale(start))
                         .attr("height", dateScale.bandwidth())
                         .attr("fill", "grey")
-                        .attr("opacity", 0.5); // Adjust the opacity to indicate moon presence
+                        .attr("opacity", 0.5);
                 }
             });
         });
 
-        // Render observations
         renderObservations();
-
-        // Render axes
         renderAxes();
     }).catch(function(error) {
         console.error("Error loading the JSON data: ", error);
     });
 }
 
-// Initial load of observations
 loadObservations();
 
-// Add event listeners to existing buttons
+// Add event listeners to buttons
 d3.select("#deleteButton").on("click", function() {
     const selectedObservations = d3.selectAll(".observation.selected");
-    const indicesToDelete = selectedObservations.nodes().map(d => +d.getAttribute("data-index"));
+    const indicesToDelete = selectedObservations.nodes().map(
+        d => +d.getAttribute("data-index")
+    );
 
     // Filter out the selected observations
-    filteredObservationData = filteredObservationData.filter((obs, i) => !indicesToDelete.includes(i));
+    filteredObservationData = filteredObservationData.filter(
+        (obs, i) => !indicesToDelete.includes(i)
+    );
 
     // Remove selected observations from the SVG
     selectedObservations.remove();
 });
 
 d3.select("#saveButton").on("click", function() {
-    // Provide updated observation.json to save
     const updatedDataStr = JSON.stringify(filteredObservationData, null, 2);
-    const dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(updatedDataStr);
+    const dataUri = (
+        'data:application/json;charset=utf-8,' +
+        encodeURIComponent(updatedDataStr)
+    );
 
     const exportFileDefaultName = 'observation.json';
 
@@ -314,9 +368,12 @@ d3.select("#fileInput").on("change", function() {
         const reader = new FileReader();
         reader.onload = function(e) {
             observationData = JSON.parse(e.target.result);
-            filteredObservationData = observationData.filter(d => dateRange.map(date => date.toISOString().split("T")[0]).includes(d.date));
-            g.selectAll(".observation").remove(); // Clear existing observation elements
-            renderObservations(); // Use the uploaded data to render observations (moon data empty to avoid conflict)
+            filteredObservationData = observationData.filter(
+                d => dateRange.map(
+                    date => date.toISOString().split("T")[0]
+                ).includes(d.date)
+            );
+            renderObservations(); // Rerender.
         };
         reader.readAsText(file);
     }
